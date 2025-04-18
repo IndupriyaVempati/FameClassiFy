37b2ef49a3fd015d5dea60834c829278b6d3eabc
IndupriyaVempati <vempatiindu744@gmail.com>
2025-04-18 14:14:40 +0530

    modified files

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import util
 import os
 from flask_cors import CORS
-app = Flask(__name__, static_folder='./ui', static_url_path='/')
+app = Flask(__name__)
 
 CORS(app)
 
